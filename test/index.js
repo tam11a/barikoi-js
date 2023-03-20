ee634fec45a74b317dd203d3bdacb5674480357c
tam11a <ibrahimsadiktamim@gmail.com>
2023-03-20 11:55:33 +0600

    updated: test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
 import Barikoi from "../dist/index.js";
 
+const BKoi = new Barikoi("NDQ0OTo0RlVHVlBYTVU5");
+
 const x = async () => {
-	const BKoi = new Barikoi("NDQ0OTo0RlVHVlBYTVU5");
 	const res = await BKoi.ReverseGeocode({
 		longitude: "90.36744611263278",
 		latitude: "23.81970957278533",
